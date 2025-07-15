83a2e0b688acf767f7d743d920715f6abefe9ac2
Mohd Shuaib <mohdshuaib0142@gmail.com>
2025-07-15 10:57:01 -0700

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,5 @@
+music  = {
+    "stealth": "https://www.youtube.com/watch?v=U47Tr9BB_wE",
+    "ehsas": "https://www.youtube.com/watch?v=U47Tr9BB_wE&list=RDU47Tr9BB_wE&start_radio=1",
+    "6 AM": "https://www.youtube.com/watch?v=I5iCw4CIpAU&list=RDU47Tr9BB_wE&index=2"
+}
